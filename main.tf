86b2c3479d09f3c3b2cf16fe1114d965e799b5de
gspence8 <gcspencer18@gmail.com>
2024-11-13 21:02:11 -0500

    Added vnet and subnet

--- main.tf
+++ main.tf
@@ -30,6 +30,21 @@ resource "azurerm_resource_group" "rg" {
   tags = local.tags
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dsba6190-cford38-dev-eastus-001"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-001"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  service_endpoints = ["Microsoft.Sql", "Microsoft.Storage"]
+}
 
 // Storage Account
 
