a1b9b87182754e19adfadb70fd57ad651e33012f
gspence8 <gcspencer18@gmail.com>
2024-11-19 17:19:24 -0500

    changed virtual network name to .vnet.name instead of rg.name

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-${random_integer.deployment_id_suffix.result}"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 
   service_endpoints = ["Microsoft.Sql", "Microsoft.Storage"]
