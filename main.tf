94ee55ee6cb44b5588334dfb8d77c290e462b7a2
gspence8 <gcspencer18@gmail.com>
2024-11-19 17:11:00 -0500

    lots of stuff

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-${random_integer.deployment_id_suffix.result}"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.rg.name
   address_prefixes     = ["10.0.2.0/24"]
 
   service_endpoints = ["Microsoft.Sql", "Microsoft.Storage"]
@@ -58,31 +58,33 @@ resource "azurerm_storage_account" "storage" {
   tags                     = local.tags
 
   network_rules {
-    default_action             = "Deny" # or "Allow"
+    default_action             = "Deny"
     virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
   }
 }
 
 resource "azurerm_mssql_server" "sql" {
-  name                         = "sqldsba6190gspence8dev${random_integer.deployment_id_suffix.result}"
+  name                         = "sql-dsba6190-gspence8-dev-${random_integer.deployment_id_suffix.result}"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = "sqladmin"     # Update with your admin username
   administrator_login_password = "P@ssword1234" # Update with a secure password
+  tags                         = local.tags
 }
 
 resource "azurerm_mssql_database" "database" {
-  name                = "dbdsba6190gspence8dev${random_integer.deployment_id_suffix.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sql.name
-  sku_name            = "Basic"
+  name         = "dbdsba6190gspence8dev${random_integer.deployment_id_suffix.result}"
+  server_id    = azurerm_mssql_server.sql.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "Basic"
+  tags         = local.tags
 }
 
 resource "azurerm_mssql_virtual_network_rule" "vnet_rule" {
-  name                = "sql-vnet-rule"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_sql_server.sql.name
-  subnet_id           = azurerm_subnet.subnet.id
+  name      = "sql-vnet-rule"
+  server_id = azurerm_sql_server.sql.id
+  subnet_id = azurerm_subnet.subnet.id
 }
